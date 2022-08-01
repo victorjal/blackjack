5315c7c164f98df24bd00a0fd069627e519adda5
Victor Armas <vjarmasl@gmail.com>
2022-08-01 04:47:23 -0700

    creating deck

---
+++ assets/js/juego.js
@@ -0,0 +1,33 @@
+/**
+ * 2C = Two of Clubs 
+ * 2C = Two of Diamonds 
+ * 2C = Two of Hearts 
+ * 2C = Two of Spades 
+ */
+
+
+let deck = [];
+const tipos = ['C','D','H','S'];
+const especiales = ['A','J','Q','K'];
+
+const crearDeck = () => {
+
+    for( let i = 2; i <= 10; i++ ) {
+        for( let tipo of tipos ) {
+            deck.push( i + tipo );
+        } 
+    }
+    
+    for( let tipo of tipos ) {
+        for( let esp of especiales ) {
+            deck.push( esp + tipo );
+        }
+    }
+
+    console.log( deck );
+    deck = _.shuffle( deck );
+    console.log( deck );
+    return deck;
+}
+
+crearDeck();
