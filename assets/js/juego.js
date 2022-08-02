1dee6942993bc7f442825cb27a4ec7d33b0d8eaf
Victor Armas <vjarmasl@gmail.com>
2022-08-02 05:16:55 -0700

    picking a card, and card values

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -1,8 +1,8 @@
 /**
  * 2C = Two of Clubs 
- * 2C = Two of Diamonds 
- * 2C = Two of Hearts 
- * 2C = Two of Spades 
+ * 2D = Two of Diamonds 
+ * 2H = Two of Hearts 
+ * 2S = Two of Spades 
  */
 
 
@@ -10,6 +10,7 @@ let deck = [];
 const tipos = ['C','D','H','S'];
 const especiales = ['A','J','Q','K'];
 
+// Esta funcion crea una nueva baraja
 const crearDeck = () => {
 
     for( let i = 2; i <= 10; i++ ) {
@@ -24,10 +25,41 @@ const crearDeck = () => {
         }
     }
 
-    console.log( deck );
+    // console.log( deck );
     deck = _.shuffle( deck );
     console.log( deck );
     return deck;
 }
 
 crearDeck();
+
+
+// Esta funcion me permite tomar una carta
+const pedirCarta = () => {
+
+    if (deck.length === 0) {
+        throw 'No hay mas cartas en el deck'
+    }
+
+    const carta = deck.pop();
+
+    console.log(deck);  // carta debe ser removida de la baraja
+    console.log(carta); // carte debe ser de la baraja
+    return carta;
+}
+
+// pedirCarta();
+
+const valorCarta = (carta) => {
+
+    const valor = carta.substring(0, carta.length - 1);
+    return ( isNaN( valor ) ) ?
+            ( valor === 'A' ) ? 11 : 10
+            : valor * 1;
+
+}
+
+const valor = valorCarta( pedirCarta() );
+console.log({ valor });
+
+
